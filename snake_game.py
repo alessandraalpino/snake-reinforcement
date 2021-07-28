4dc4de2c96ba72bc01c74e48cea9a051ec967a67
Alessandra Alpino Costa <alessandraalpino@hotmail.com>
2021-07-28 11:13:41 -0300

    add comentários no código

--- snake_game.py
+++ snake_game.py
@@ -13,10 +13,11 @@ class Direction(Enum):
 
 Point = namedtuple('Point','x,y')
 
+# set block size
 BLOCK_SIZE = 20
 
 class SnakeGame:
-
+    # set display size
     def __init__(self,w=640,h=480):
         self.w = w
         self.h = h
@@ -26,24 +27,31 @@ class SnakeGame:
         pygame.display.set_caption('Snake')
         self.clock = pygame.time.Clock()
 
-
         # init game state
         self.direction = Direction.RIGHT
-
+        # put snake in the center of the display
         self.head = Point(self.w/2, self.h/2)
+        # creating the initial snake with the head plus 2 blocks for the body
         self.snake = [self.head,
                         Point(self.head.x-BLOCK_SIZE,self.head.y),
                         Point(self.head.x-(2*BLOCK_SIZE),self.head.y)]
 
+        # set initial score to 0 and no food
         self.score = 0
         self.food = None
+        # place food in the display
         self._place_food()
 
     def _place_food(self):
+        # food will be a random point inside the display size
+        # self.w - BLOCK_SIZE -> to ensure that the point will be inside the display
+        # we use // BLOCK_SIZE to garantee that the max value will be multiple of BLOCK_SIZE
+        # (self.w - BLOCK_SIZE) // BLOCK_SIZE ) this represents the amount of blocks that fit the display
+        # * BLOCK_SIZE is to get the final value in pixels
         x = random.randint(0, (self.w - BLOCK_SIZE) // BLOCK_SIZE ) * BLOCK_SIZE
         y = random.randint(0, (self.h - BLOCK_SIZE) // BLOCK_SIZE ) * BLOCK_SIZE
         self.food = Point(x,y)
-        # ver se a comida não está dentro da snake
+        # check if food is inside the snake
         if self.food in self.snake:
             self.place_food()
 
